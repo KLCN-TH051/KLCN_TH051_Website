86d8b23353a90235855bec66bcfa1ab654001df6
Giang Tuấn Kiệt <giangkiet2k4@gmail.com>
2025-11-12 15:33:21 +0700

    Layout

--- KLCN_TH051_Web.WebApplication/Program.cs
+++ KLCN_TH051_Web.WebApplication/Program.cs
@@ -24,4 +24,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "admin",
+    pattern: "Admin/{controller=Home}/{action=Index}/{id?}",
+    defaults: new { area = "Admin" });
+
 app.Run();
