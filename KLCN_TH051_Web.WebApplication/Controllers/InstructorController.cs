28455fa30a16f87e207094c8d6b9e15bcc7ba7d7
Giang Tuấn Kiệt <giangkiet2k4@gmail.com>
2025-11-14 15:25:59 +0700

    Giảng viên

---
+++ KLCN_TH051_Web.WebApplication/Controllers/InstructorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KLCN_TH051_Web.WebApplication.Controllers
+{
+    public class InstructorController : Controller
+    {
+        public IActionResult Statistics()
+        {
+            ViewData["ActiveMenu"] = "stats";
+            return View();
+        }
+
+        public IActionResult Courses()
+        {
+            ViewData["ActiveMenu"] = "courses";
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            ViewData["ActiveMenu"] = "orders";
+            return View();
+        }
+    }
+}
