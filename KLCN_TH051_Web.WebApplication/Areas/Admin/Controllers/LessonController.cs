3a76099ffb035de9375779900f58b514a3255fcc
Giang Tuấn Kiệt <giangkiet2k4@gmail.com>
2025-11-17 15:34:24 +0700

    Môn học

---
+++ KLCN_TH051_Web.WebApplication/Areas/Admin/Controllers/LessonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KLCN_TH051_Web.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LessonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Banners()
+        {
+            return View();
+        }
+    }
+}
