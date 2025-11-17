cd5eb24646da2a0ccd3e04ff29873538faa19fcd
Drunken2004 <trandrk@gmail.com>
2025-11-17 14:07:37 +0700

    Add Instructor Controller and View

---
+++ KLCN_TH051_Web.WebApplication/Areas/Instructor/Controllers/Course.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KLCN_TH051_Web.WebApplication.Areas.Instructor.Controllers
+{
+    public class Course : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
