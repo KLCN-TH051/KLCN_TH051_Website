a6afe9a08982334e1f95c11d0e3efaf02811c585
Drunken2004 <trandrk@gmail.com>
2025-11-18 02:26:56 +0700

    add controller, view ranking, profile, logout

---
+++ KLCN_TH051_Web.WebApplication/Areas/Instructor/Controllers/RankingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KLCN_TH051_Web.WebApplication.Areas.Instructor.Controllers
+{
+    [Area("Instructor")]
+    public class RankingController : Controller
+    {
+        // Trang chọn khóa học để xem xếp hạng
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Xếp hạng học viên";
+            return View();
+        }
+
+        // Trang hiển thị bảng xếp hạng của 1 khóa học cụ thể
+        public IActionResult CourseRanking(int courseId)
+        {
+            ViewData["Title"] = "Bảng xếp hạng học viên";
+            ViewBag.CourseId = courseId;
+            return View();
+        }
+    }
+}
