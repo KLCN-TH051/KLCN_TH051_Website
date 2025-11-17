19c5fd607813e39f3337671f0891aeee930149b8
VoThiHien2501 <hienvothi033@gmail.com>
2025-11-17 14:06:04 +0700

    tạo bang banner

---
+++ KLCN_TH051_Web.Repositories/Migrations/20251117065350_AddBannerTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KLCN_TH051_Web.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBannerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Banners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LinkUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastUpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeletedTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banners", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Banners");
+        }
+    }
+}
---
+++ KLCN_TH051_Website.Common/Entities/Banner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLCN_TH051_Website.Common.Entities
+{
+    public class Banner : BaseEntity
+    {
+        public int Id { get; set; }
+
+        // Tiêu đề banner
+        public string Title { get; set; }
+
+        // URL hình ảnh banner
+        public string ImageUrl { get; set; }
+
+        // Link khi click vào banner
+        public string? LinkUrl { get; set; }
+
+        // Thứ tự hiển thị (nếu có nhiều banner)
+        public int Order { get; set; }
+
+        // Trạng thái banner: Active / Inactive
+        public bool IsActive { get; set; } = true;
+    }
+}
