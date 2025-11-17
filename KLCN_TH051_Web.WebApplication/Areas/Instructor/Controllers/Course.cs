3e45e93edf0844fbd5cbb8f47437332116be9f7f
VoThiHien2501 <hienvothi033@gmail.com>
2025-11-17 14:15:32 +0700

    them

--- KLCN_TH051_Web.WebApplication/Areas/Instructor/Controllers/Course.cs
+++ KLCN_TH051_Web.WebApplication/Areas/Instructor/Controllers/Course.cs
@@ -2,10 +2,12 @@
 
 namespace KLCN_TH051_Web.WebApplication.Areas.Instructor.Controllers
 {
+    [Area("Instructor")]
     public class Course : Controller
     {
         public IActionResult Index()
         {
+            ViewData["Title"] = "Khoá học giảng dạy";
             return View();
         }
     }
