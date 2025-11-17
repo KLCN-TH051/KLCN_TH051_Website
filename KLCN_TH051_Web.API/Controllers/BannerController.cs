c0df2313c8bfb5ad164014003e5c733d0a984591
VoThiHien2501 <hienvothi033@gmail.com>
2025-11-17 16:50:31 +0700

    viet api cho banner

---
+++ KLCN_TH051_Web.API/Controllers/BannerController.cs
@@ -0,0 +1,48 @@
+﻿using KLCN_TH051_Website.Common.DTO.Requests;
+using KLCN_TH051_Website.Common.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KLCN_TH051_Web.API.Controllers
+{
+    [Route("api/banners")]
+    [ApiController]
+    public class BannerController : ControllerBase
+    {
+        private readonly IBannerService _bannerService;
+
+        public BannerController(IBannerService bannerService)
+        {
+            _bannerService = bannerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _bannerService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateBannerRequest request)
+        {
+            return Ok(await _bannerService.CreateAsync(request));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateBannerRequest request)
+        {
+            return Ok(await _bannerService.UpdateAsync(id, request));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _bannerService.DeleteAsync(id));
+        }
+
+        [HttpPut("{id}/reorder/{newOrder}")]
+        public async Task<IActionResult> Reorder(int id, int newOrder)
+        {
+            return Ok(await _bannerService.ReorderAsync(id, newOrder));
+        }
+    }
+}
---
+++ KLCN_TH051_Website.Common/Interfaces/IBannerService.cs
@@ -0,0 +1,19 @@
+﻿using KLCN_TH051_Website.Common.DTO.Requests;
+using KLCN_TH051_Website.Common.DTO.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLCN_TH051_Website.Common.Interfaces
+{
+    public interface IBannerService
+    {
+        Task<BannerResponse> CreateAsync(CreateBannerRequest request);
+        Task<BannerResponse> UpdateAsync(int id, UpdateBannerRequest request);
+        Task<bool> DeleteAsync(int id);
+        Task<List<BannerResponse>> GetAllAsync();
+        Task<bool> ReorderAsync(int id, int newOrder);
+    }
+}
