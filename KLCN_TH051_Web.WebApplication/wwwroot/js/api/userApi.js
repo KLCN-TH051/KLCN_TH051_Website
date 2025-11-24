d3b3f43116d4b357eaff93e0894ab3f7808c3d7b
VoThiHien2501 <hienvothi033@gmail.com>
2025-11-24 19:02:13 +0700

    gọi api user

---
+++ KLCN_TH051_Web.WebApplication/wwwroot/js/api/userApi.js
@@ -0,0 +1,35 @@
+﻿import BaseApi from "./baseApi";
+
+const UserApi = {
+    // Lấy tất cả tài khoản
+    getAll: () => {
+        return BaseApi.get("users");
+    },
+
+    // Lấy chi tiết tài khoản theo Id
+    getById: (id) => {
+        return BaseApi.get(`users/${id}`);
+    },
+
+    // Tạo tài khoản mới
+    create: (data) => {
+        return BaseApi.post("users", data);
+    },
+
+    // Cập nhật tài khoản theo Id
+    update: (id, data) => {
+        return BaseApi.put(`users/${id}`, data);
+    },
+
+    // Xóa tài khoản theo Id
+    delete: (id) => {
+        return BaseApi.delete(`users/${id}`);
+    },
+
+    // Lấy danh sách giáo viên
+    getTeachers: () => {
+        return BaseApi.get("users/teachers");
+    }
+};
+
+export default UserApi;
