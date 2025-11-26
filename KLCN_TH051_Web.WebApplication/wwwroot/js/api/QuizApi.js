f870e8195707808fc7ff4f936069b0fe1c0b3964
Giang Tuấn Kiệt <giangkiet2k4@gmail.com>
2025-11-26 10:42:30 +0700

    quiz

---
+++ KLCN_TH051_Web.WebApplication/wwwroot/js/api/QuizApi.js
@@ -0,0 +1,30 @@
+﻿import BaseApi from "../core/BaseApi.js";
+
+const QuizApi = {
+    async getQuiz(lessonId, data) {
+        try {
+            return await BaseApi.get(`Quiz/lesson/${lessonId}`, data);
+        } catch (err) {
+            console.error("Lỗi getQuiz:", err);
+            throw err;
+        }
+    },
+    async update(id, data) {
+        try {
+            return await BaseApi.put(`Quiz/${id}`, data);
+        } catch (err) {
+            console.error("Lỗi update:", err);
+            throw err;
+        }
+    },
+    async delete(id) {
+        try {
+            return await BaseApi.delete(`Quiz/${id}`);
+        } catch (err) {
+            console.error("Lỗi delete:", err);
+            throw err;
+        }
+    },
+}
+
+export default QuizApi;
